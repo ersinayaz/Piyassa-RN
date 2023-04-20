8b5f444708438b3d480455d9b8d61e62c8b0a620
Ersin Ayaz <ersinayaz@gmail.com>
2023-04-20 03:11:21 +0300

    ParityCard added.
    -ParityCard component added.
    -ParityPrice component added.

--- src/components/ParityCard.tsx
+++ src/components/ParityCard.tsx
@@ -1,28 +1,22 @@
-import { useStore } from '../store';
+import ParityPrice from './ParityPrice';
 import { color } from '../assets/colors';
-import React, { useState, useEffect } from 'react';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-const ParityCard = ({ navigation, data }) => {
-
+export default ParityCard = ({ navigation, data }) => 
+{
     return (
         <TouchableOpacity style={styles.container}>
-            <View>
-                <Image style={styles.image} source={data.image} />
-            </View>
+            <Image style={styles.image} source={data.image} />
             <View style={styles.center}>
                 <Text style={styles.title}>{data.name}</Text>
                 <Text style={styles.description}>{data.description}</Text>
             </View>
             <View style={styles.right}>
-                <Text>{data.closingPrice}</Text>
+                <ParityPrice data={data} navigarion={navigation} />
             </View>
         </TouchableOpacity>
     );
 };
-
-export default ParityCard;
 
 const styles = StyleSheet.create({
     container: {
---
+++ src/components/ParityPrice.tsx
@@ -0,0 +1,141 @@
+import { useStore } from '../store';
+import { useFirestore } from '../db';
+import { color } from '../assets/colors';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import React, { useState, useEffect, useMemo } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+interface ParityPriceProps {
+    data: any;
+    navigation: any;
+}
+
+enum ChangeState {
+    up = "up",
+    down = "down",
+    equal = "equal",
+}
+
+const ParityPrice = (props: ParityPriceProps) => {
+    const { parities } = useFirestore();
+    const calculateRate = (lastPrice) => {
+        const changeRate = Math.abs(((props.data.closingPrice - lastPrice) / lastPrice) * 100).toFixed(2);
+        const state = props.data.closingPrice < lastPrice ? ChangeState.up : props.data.closingPrice > lastPrice ? ChangeState.down : ChangeState.equal;
+        const rate = lastPrice == props.data.closingPrice ? 0 : (changeRate === '0.00' ? '0.01' : changeRate);
+        const style = state === ChangeState.up ? styles.up : state === ChangeState.down ? styles.down : styles.equal;
+
+        return { rate, state, style };
+    };
+
+    const [data, setData] = useState(props.data);
+    const [lastPrice, setLastPrice] = useState({ price: props.data.price, state: ChangeState.equal, style: styles.equal });
+    const [changeRate, setChangeRate] = useState(calculateRate(props.data.price));
+
+
+    useEffect(() => {
+        const onPriceChange = (price) => {
+            // console.log("onPriceChane", price);
+            props.data.price = price;
+            let state = ChangeState.equal;
+            let style = styles.equal;
+            if (price > lastPrice.price) {
+                state = ChangeState.up;
+                style = styles.up;
+            } else if (price < lastPrice.price) {
+                state = ChangeState.down;
+                style = styles.down;
+            }
+            setLastPrice({ price, state, style });
+            setChangeRate(calculateRate(price));
+        }
+
+        const onClosingPriceChange = (closingPrice) => {
+            // console.log("onClosingPriceChange", closingPrice);
+            props.data.closingPrice = closingPrice;
+            setLastPrice({ price: closingPrice, state: ChangeState.equal, style: styles.equal });
+            setChangeRate({ rate: 0, state: ChangeState.equal, style: styles.equal });
+        }
+
+        if (data.price !== lastPrice.price) {
+            onPriceChange(data.price);
+        }
+        if (data.closingPrice !== props.data.closingPrice) {
+            onClosingPriceChange(data.closingPrice);
+        }
+    }, [data]);
+
+
+    useEffect(() => {
+        const unsubscribe = parities.onChangeById(props.data.id, (data) => {
+            setData(data);
+        });
+
+        return () => {
+            unsubscribe();
+        };
+    }, [props.data.id, parities]);
+
+
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.lastPrice}>
+                {
+                    lastPrice.state !== ChangeState.equal ?
+                        <Ionicons name={`ios-caret-${lastPrice.state}`} size={14} style={lastPrice.style} />
+                        : null
+                }
+                <Text style={[styles.price, lastPrice.style]}>{lastPrice.price.toFixed(4)}<Text style={[styles.priceChar, lastPrice.style]}>{props.data.priceChar}</Text></Text>
+            </View>
+            <View style={styles.changeRate}>
+                <Text style={[styles.rate, changeRate.style]}>%{changeRate.rate}</Text>
+                <Image style={styles.icon} source={require('../assets/images/general/24-hours.png')} />
+            </View>
+        </View>
+    );
+};
+
+export default ParityPrice;
+
+const styles = StyleSheet.create({
+    container: {
+    },
+    lastPrice: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+    },
+    changeRate: {
+        marginTop: 5,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+    },
+    icon: {
+        width: 14,
+        height: 14,
+        marginLeft: 5,
+    },
+    rate: {
+        fontSize: 10,
+        fontWeight: '500'
+    },
+    price: {
+        fontSize: 16,
+        marginLeft: 5,
+        fontWeight: 'bold'
+    },
+    priceChar: {
+        fontSize: 12,
+        fontWeight: '500',
+    },
+    up: {
+        color: "#00B75F",
+    },
+    down: {
+        color: "#D80027",
+    },
+    equal: {
+        color: color("color7"),
+    },
+});
